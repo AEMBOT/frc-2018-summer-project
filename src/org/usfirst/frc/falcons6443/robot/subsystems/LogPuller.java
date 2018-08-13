f45b4c9a3a421fc2a6b7ea8416b832f367326370
OwenEngbretson <owenengbretson@gmail.com>
2018-08-12 17:50:33 -0700

    Added some automation features
    Added for auto calling of .bat file and check for warranting the call of LogRetrieval based upon memory on the RIO.
    DID NOT add to build file

--- src/org/usfirst/frc/falcons6443/robot/subsystems/LogPuller.java
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/LogPuller.java
@@ -2,21 +2,41 @@ package org.usfirst.frc.falcons6443.robot.subsystems;
 
 import java.io.File;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class LogPuller {
 
     //Booleans used to specify weather or not the USB Drive and the robot are connected
     static boolean isUSBConnected = false;
     static boolean isRobotConnected = false;
+    static Process process;
 
     public static void main(String[] args) {
 
         String drivePath = "F:\\";
+        Path rioPath = Paths.get("/home");
+        double rioMem = 0;
+
+        try {
+            rioMem = getUsableSpace(rioPath);
+        } catch (Exception e){
+            System.out.println("Couldn't access available rio memory");
+        }
+
+
+
        isUSBConnected = checkIfDirectoryExists(drivePath);
 
-       if(isUSBConnected && isRobotConnected)
+       if(isUSBConnected && isRobotConnected && (rioMem != 0 && rioMem > 5))
        {
             //When both are connected run batch file
+         try {
+             process = Runtime.getRuntime().exec("loggerRetrieval.bat");
+         }catch (IOException e){
+             System.out.println("File couldn't / didn't run");
+         }
        }
        else
        {
@@ -40,4 +60,12 @@ public class LogPuller {
         }
         return isUSBConnected;
     }
+
+
+    //Returns total unused space of param (RIO) in MB
+    private static double getUsableSpace(Path path) throws Exception{
+       return Files.getFileStore(path).getTotalSpace() / 1048576;
+    }
+
+
 }
