675cee28b710036ee4638ec7aa2650fa281a14be
Will <jwilt9000@gmail.com>
2018-08-13 11:28:12 -0700

    added descriptive comments

--- src/org/usfirst/frc/falcons6443/robot/utilities/LogPuller.java
+++ src/org/usfirst/frc/falcons6443/robot/utilities/LogPuller.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.falcons6443.robot.utilities;
 
-import org.apache.tools.ant.Task;
 
 import java.io.File;
 import java.io.*;
@@ -8,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class LogPuller extends Task {
+public class LogPuller {
 
     //Booleans used to specify weather or not the USB Drive and the robot are connected
     private boolean isUSBConnected = false;
@@ -17,21 +16,21 @@ public class LogPuller extends Task {
 
     public void execute() {
 
+        //USB drive path and rio home path variables
         String drivePath = "F:\\";
         Path rioPath = Paths.get("/home");
+
+        //Rio Memory
         double rioMem = 0;
 
-        log("BOOLIN");
-
-
+        //Attempts to set amount of usable memory on the RIO
         try {
             rioMem = getUsableSpace(rioPath);
         } catch (Exception e){
-            log("Couldn't access available rio memory");
+            System.out.println("Couldn't access available rio memory");
         }
 
-
-
+        //Sets variable depending on if robot is connected or not
        isUSBConnected = checkIfDirectoryExists(drivePath);
 
        if(isUSBConnected && isRobotConnected && (rioMem != 0 && rioMem > 5))
@@ -40,12 +39,12 @@ public class LogPuller extends Task {
          try {
              process = Runtime.getRuntime().exec("loggerRetrieval.bat");
          }catch (IOException e){
-             log("File couldn't / didn't run");
+             System.out.println("File couldn't / didn't run");
          }
        }
        else
        {
-            log("USB or Robot not connected. Please Try again");
+           System.out.println("USB or Robot not connected. Please Try again");
        }
     }
 
