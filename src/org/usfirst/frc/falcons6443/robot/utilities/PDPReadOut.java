fc27fa389dc77b1d83cea67d73963359c96d9431
Will <jwilt9000@gmail.com>
2018-08-21 15:21:07 -0700

    added functionality to PDPReadOut

--- src/org/usfirst/frc/falcons6443/robot/utilities/PDPReadOut.java
+++ src/org/usfirst/frc/falcons6443/robot/utilities/PDPReadOut.java
@@ -1,8 +1,15 @@
 package org.usfirst.frc.falcons6443.robot.utilities;
 
+import edu.wpi.first.wpilibj.hal.PDPJNI;
+
 public class PDPReadOut {
 
-    public void execute() {
+    public double execute(int portNum) {
 
+        double voltage = PDPJNI.getPDPVoltage(portNum);
+
+        return voltage;
     }
+
+
 }
